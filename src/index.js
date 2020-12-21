5366fb80341925c321e5b0aed73dd5bd5f66dd18
Bogdan <bogdankuzzmin@gmail.com>
2020-12-21 23:36:57 +0600

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react';
+import React, {Component, useState, useContext, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 import '../src/index.css';
@@ -17,7 +17,7 @@ const App = () => {
 const TestContextC = () => {
   const value = useContext(TestContext);
 
-  return <p>{value}</p>
+  return <p style={{marginTop: '50px'}}>{value}</p>
 };
 
 const HookSwitcher = () => {
@@ -58,8 +58,56 @@ const HookSwitcher = () => {
       </button>
 
       <TestContextC />
+      <TestUseEffect />
     </div>
   )
+}
+
+const TestUseEffect = () => {
+  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  if (visible) {
+    return (
+      <div style={{marginTop: '50px'}}>
+        <button onClick={() => setValue((v) => v + 1)}>+</button>
+        <button onClick={() => setVisible(false)}>hide</button>
+
+        <ClassCounter value={value} />
+        <HookCounter value={value} />
+      </div>
+    )
+  } else {
+    return <button onClick={() => setVisible(true)}>show</button>
+  }
+};
+
+const HookCounter = ({value}) => {
+  useEffect(() => {
+    console.log('useEffect()');
+
+    return () => console.log('clear');
+  }, [value]);
+
+  return <p>{value}</p>
+};
+
+class ClassCounter extends Component {
+  componentDidMount() {
+    console.log('class: mount');
+  }
+
+  componentDidUpdate() {
+    console.log('class: update');
+  }
+
+  componentWillUnmount() {
+    console.log('class: unmount');
+  }
+
+  render() {
+    return <p>{this.props.value}</p>;
+  }
 }
 
 ReactDOM.render(<App />,
